346c53004a42ec6d010d0eeb89c45e4712652f40
Alexis Morand <alexis@slash-lab.fr>
2017-02-22 18:57:08 +0100

    Page: waiting page integration (inc. responsive)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    body_classes 'wip'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def body_classes(css = nil)
+    @body_classes ||= []
+    if css.present?
+      @body_classes << css
+    else
+      css = @body_classes
+      # css << "old_browser" unless browser.modern?
+      css.any? ? (' class="' + css.join(' ').to_s + '"').html_safe : ""
+    end
+  end
+
+  def meta_title(title = false)
+    @meta_title ||= []
+    if title.present?
+      @meta_title << title
+    else
+      title = @meta_title
+      # title <<  t('.subtitle') if title.count < 2
+      title << "Le Hasard Ludique"
+      title.join(' | ')
+    end
+  end
 end
