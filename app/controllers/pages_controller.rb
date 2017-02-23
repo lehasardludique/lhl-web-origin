b5f28d88e63168c4945cf44bb04d4eac3623c21d
Alexis Morand <alexis@slash-lab.fr>
2017-02-23 14:51:52 +0100

    Redirection to _LaFabrique_

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     body_classes 'wip'
   end
+
+  def redirect
+    redirect_to "//lafabrique.lehasardludique.paris/#{params[:slug]}", status: :moved_permanently
+  end
 end
