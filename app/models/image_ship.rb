72298157308a668b1bc64d643ab0ad8d1d0f0208
Alexis Morand <alexis@slash-lab.fr>
2017-02-24 22:02:31 +0100

    ImageShip: validations

--- app/models/image_ship.rb
+++ app/models/image_ship.rb
@@ -1,4 +1,7 @@
 class ImageShip < ApplicationRecord
   belongs_to :gallery
   belongs_to :resource
+
+  validates :gallery_id, presence: true, numericality: { only_integer: true }
+  validates :resource_id, presence: true, numericality: { only_integer: true }
 end
