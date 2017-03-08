8cb5e32915d0664f9abea287ca6b0f9b5b729651
Alexis Morand <alexis@slash-lab.fr>
2017-03-08 22:28:03 +0100

    [R] Admin: catch error impossible deletion (linked resource)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ApplicationHelper
 
+  rescue_from PG::ForeignKeyViolation, with: :ooops!
+
   after_action :store_location
 
   def not_found!
@@ -11,6 +13,11 @@ class ApplicationController < ActionController::Base
     @title = t 'errors.not_found.title'
     @text = t 'errors.not_found.text'
     render :error, status: :not_found and return
+  end
+
+  def ooops!
+    flash[:error] = "La resource à effacer est liée à un autre objet. Merci de supprimer cette liaison avant de réessayer."
+    redirect_back(fallback_location: session_path)
   end
 
   private
