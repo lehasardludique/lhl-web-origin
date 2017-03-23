6d4de858c87e5b59aeb0628e44289220b5a275e8
Alexis Morand <alexis@slash-lab.fr>
2017-03-23 21:18:28 +0100

    HomeCarouselLinks: `ckeck_rank_consistency`

--- app/models/home_carousel_link.rb
+++ app/models/home_carousel_link.rb
@@ -5,11 +5,14 @@ class HomeCarouselLink < ApplicationRecord
   enum status: { draft: 0, published: 1 }
 
   attr_reader :final_title, :final_subtitle, :final_resource, :slug
+  attr_accessor :form
 
   validates :home_linkable_type, presence: true
   validates :home_linkable_id, presence: true, numericality: { only_integer: true }
 
   default_scope { order(rank: :asc) }
+
+  after_save :check_rank_consistency
 
   def self.types
     [ Article, Page ]
@@ -58,5 +61,15 @@ class HomeCarouselLink < ApplicationRecord
 
     def get_final_slug
      (defined? self.home_linkable.slug) ? self.home_linkable.slug : '#'
+    end
+
+    def check_rank_consistency
+      if self.rank_changed? and not self.form == :callback
+        order_direction = self.rank > self.rank_was ? :asc : :desc
+        HomeCarouselLink.all.reorder(rank: :asc, updated_at: order_direction).each_with_index do |hcl, i|
+          hcl.form = :callback
+          hcl.update(rank: i + 1) unless hcl.rank == i + 1
+        end
+      end
     end
 end
