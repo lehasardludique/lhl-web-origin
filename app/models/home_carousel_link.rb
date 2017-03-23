2067eaaff4b773e8e5f05d7178291890609554dd
Alexis Morand <alexis@slash-lab.fr>
2017-03-23 16:12:17 +0100

    HomeCarouselLinks: `final_*` attributes/methods

--- app/models/home_carousel_link.rb
+++ app/models/home_carousel_link.rb
@@ -1,7 +1,10 @@
 class HomeCarouselLink < ApplicationRecord
   belongs_to :home_linkable, polymorphic: true
+  belongs_to :resource
 
   enum status: { draft: 0, published: 1 }
+
+  attr_reader :final_title, :final_subtitle, :final_resource
 
   validates :home_linkable_type, presence: true
   validates :home_linkable_id, presence: true, numericality: { only_integer: true }
@@ -11,4 +14,41 @@ class HomeCarouselLink < ApplicationRecord
   def self.types
     [ Article, Page ]
   end
+
+  def final_title
+    @final_title ||= get_final_title()
+  end
+
+  def final_subtitle
+    @final_subtitle ||= get_final_subtitle()
+  end
+
+  def final_resource
+    @final_resource ||= get_final_resource()
+  end
+
+  private
+    def get_final_title
+      if self.title.present?
+        self.title
+      else
+       (defined? self.home_linkable.title) ? self.home_linkable.title : nil
+      end
+    end
+
+    def get_final_subtitle
+      if self.subtitle.present?
+        self.subtitle
+      else
+       (self.title.blank? and defined? self.home_linkable.subtitle) ? self.home_linkable.subtitle : nil
+      end
+    end
+
+    def get_final_resource
+      if self.resource.present?
+        self.resource
+      else
+       (defined? self.home_linkable.main_picture) ? self.home_linkable.main_picture : nil
+      end
+    end
 end
