a22212d61137565650af04e8250b366dfa917f29
Alexis Morand <alexis@slash-lab.fr>
2017-03-23 21:25:25 +0100

    ImageShips: improve `ckeck_rank_consistency`

--- app/models/image_ship.rb
+++ app/models/image_ship.rb
@@ -2,6 +2,8 @@ class ImageShip < ApplicationRecord
   belongs_to :gallery
   belongs_to :resource
 
+  attr_accessor :form
+
   validates :gallery_id, presence: true, numericality: { only_integer: true }
   validates :resource_id, presence: true, numericality: { only_integer: true }
 
@@ -11,12 +13,11 @@ class ImageShip < ApplicationRecord
 
   private
     def check_rank_consistency
-      if self.rank_changed? or self.id_changed?
-        switched_image_ship = ImageShip.where.not(id: self.id).find_by(gallery_id: self.gallery_id, rank: self.rank)
-        if switched_image_ship
-          switched_image_ship_new_rank = (self.id_changed?) ? ImageShip.where(gallery_id: self.gallery_id).count : self.rank_was
-          Rails.logger.info "----> NEW RANK #{switched_image_ship_new_rank}"
-          switched_image_ship.update( rank: switched_image_ship_new_rank )
+      if self.rank_changed? and not self.form == :callback
+        order_direction = self.rank > self.rank_was ? :asc : :desc
+        ImageShip.where(gallery_id: self.gallery_id).reorder(rank: :asc, updated_at: order_direction).each_with_index do |is, i|
+          is.form = :callback
+          is.update(rank: i + 1) unless is.rank == i + 1
         end
       end
     end
