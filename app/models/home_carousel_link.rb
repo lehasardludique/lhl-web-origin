e03a2452a88adc774c5b69e6ac8151eb3de01ac6
Alexis Morand <alexis@slash-lab.fr>
2017-03-23 15:28:03 +0100

    HomeCarouselLinks: validations  & `self.types`

--- app/models/home_carousel_link.rb
+++ app/models/home_carousel_link.rb
@@ -3,5 +3,12 @@ class HomeCarouselLink < ApplicationRecord
 
   enum status: { draft: 0, published: 1 }
 
+  validates :home_linkable_type, presence: true
+  validates :home_linkable_id, presence: true, numericality: { only_integer: true }
+
   default_scope { order(rank: :asc) }
+
+  def self.types
+    [ Article, Page ]
+  end
 end
