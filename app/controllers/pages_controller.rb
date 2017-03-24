22099463fc1535d5e7d7ab1630ff7929397ddfc7
Alexis Morand <alexis@slash-lab.fr>
2017-03-24 18:30:55 +0100

    TradeSpotting: back to good home pixel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @home_carousels_links = HomeCarouselLink.published
-    @retargeting_pixel_id = 8020083
+    @retargeting_pixel_id = 8147450
     body_classes 'home'
   end
 
