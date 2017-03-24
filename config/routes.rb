f5f16827eee16a6cf3e07bdbfb6df2bc9ebd5c48
Alexis Morand <alexis@slash-lab.fr>
2017-03-24 12:53:21 +0100

    RC: active home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#wip'
-  get 'home' => 'pages#home'
+  root 'pages#home'
+  # get 'home' => 'pages#home'
   
   # UserSessions
   get 'connexion' => 'user_sessions#new', as: :login
