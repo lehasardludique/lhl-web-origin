dc3facc7cc9a1b92457f4c53f5521e5094b4374f
Alexis Morand <alexis@slash-lab.fr>
2017-04-20 11:31:56 +0200

    ArtistEventLink: associations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,8 @@
 class Artist < ApplicationRecord
   belongs_to :user
   belongs_to :resource
+  has_many :artist_event_links
+  has_many :events, -> { reorder(start_time: :desc) }, through: :artist_event_links
   enum status: { draft: 0, published: 1 }
 
   validates :name, presence: true
