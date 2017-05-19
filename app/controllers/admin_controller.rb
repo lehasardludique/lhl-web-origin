4a0198ae6a3fd11f7804806df8f897bc57f68012
Alexis Morand <alexis@slash-lab.fr>
2017-05-19 17:40:56 +0200

    AdminController: clean comments

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,9 +7,5 @@ class AdminController < ApplicationController
         store_location
         redirect_to login_path and return
       end
-      # unless current_user.admin?
-      #   flash.alert = "L'interface d'administration est réservée aux administrateurs."
-      #   redirect_to root_path
-      # end
     end
 end
