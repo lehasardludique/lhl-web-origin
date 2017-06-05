4f20075b739478754c1f3be6b3eac50204dd1cd7
Alexis Morand <alexis@slash-lab.fr>
2017-06-05 15:38:06 +0200

    Home: add responsive trailer card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @home_carousels_links = HomeCarouselLink.published
+    @events = Event.next.limit(6)
     @retargeting_pixel_id = ENV['KLOX_HOME_PIXEL'] if ENV['KLOX_HOME_PIXEL']
     body_classes 'home'
   end
