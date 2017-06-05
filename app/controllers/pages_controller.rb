a5b0cf404bd16f94343ee7e2d68ff3ba995e2ae6
Alexis Morand <alexis@slash-lab.fr>
2017-06-05 15:43:27 +0200

    Home: event trailer card order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @home_carousels_links = HomeCarouselLink.published
-    @events = Event.next.limit(6)
+    @events = Event.next.limit(6).reorder(end_time: :asc)
     @retargeting_pixel_id = ENV['KLOX_HOME_PIXEL'] if ENV['KLOX_HOME_PIXEL']
     body_classes 'home'
   end
