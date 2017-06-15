b04f210fa9e3e6f167a1b9d65ba24a0dd0c7f86d
Alexis Morand <alexis@slash-lab.fr>
2017-06-15 14:55:07 +0200

    PartnersPage: model & local

---
+++ app/models/partners_page.rb
@@ -0,0 +1,57 @@
+class PartnersPage
+  include ActiveModel::Model
+  attr_accessor :base_line, :section_1, :section_1_ids, :section_2, :section_2_ids, :section_3, :section_3_ids
+
+  def initialize(attributes={})
+    super
+    @base_line     ||= SingleData.partners_baseline.v
+    @section_1     ||= SingleData.partners_section_1.v
+    @section_1_ids ||= SingleData.partners_section_1_ids.v
+    @section_2     ||= SingleData.partners_section_2.v
+    @section_2_ids ||= SingleData.partners_section_2_ids.v
+    @section_3     ||= SingleData.partners_section_3.v
+    @section_3_ids ||= SingleData.partners_section_3_ids.v
+  end
+
+  def update(attributes={})
+    if "base_line".in? attributes.keys
+      success ||= 1
+      success *= SingleData.partners_baseline.update(v: attributes[:base_line]) ? 1 : 0
+    end
+    if "section_1".in? attributes.keys
+      success ||= 1
+      success *= SingleData.partners_section_1.update(v: attributes[:section_1]) ? 1 : 0
+    end
+    if "section_1_ids".in? attributes.keys
+      success ||= 1
+      success *= SingleData.partners_section_1_ids.update(v: attributes[:section_1_ids]) ? 1 : 0
+    end
+    if "section_2".in? attributes.keys
+      success ||= 1
+      success *= SingleData.partners_section_2.update(v: attributes[:section_2]) ? 1 : 0
+    end
+    if "section_2_ids".in? attributes.keys
+      success ||= 1
+      success *= SingleData.partners_section_2_ids.update(v: attributes[:section_2_ids]) ? 1 : 0
+    end
+    if "section_3".in? attributes.keys
+      success ||= 1
+      success *= SingleData.partners_section_3.update(v: attributes[:section_3]) ? 1 : 0
+    end
+    if "section_3_ids".in? attributes.keys
+      success ||= 1
+      success *= SingleData.partners_section_3_ids.update(v: attributes[:section_3_ids]) ? 1 : 0
+    end
+    result = success == 1 ? true : false
+  end
+
+  def save
+    SingleData.partners_baseline.update(v: self.base_line)
+    SingleData.partners_section_1.update(v: self.section_1)
+    SingleData.partners_section_1_ids.update(v: self.section_1_ids)
+    SingleData.partners_section_2.update(v: self.section_2)
+    SingleData.partners_section_2_ids.update(v: self.section_2_ids)
+    SingleData.partners_section_3.update(v: self.section_3)
+    SingleData.partners_section_3_ids.update(v: self.section_3_ids)
+  end
+end 
