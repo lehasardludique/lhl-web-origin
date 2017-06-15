fb6f175380b0367de5cf31b08a3f9b0ea861180b
Alexis Morand <alexis@slash-lab.fr>
2017-06-15 19:34:33 +0200

    ApplicationHelper: update `render_header` method (inc. PartnersPage)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,7 +54,7 @@ module ApplicationHelper
   end
 
   def render_header object
-    if object.class.in? [ Page, Festival, Article, Event ]
+    if object.class.in? [ Page, Festival, Article, Event , PartnersPage ]
       if object.main_gallery.present?
         render_gallery object.main_gallery, :diaporama
       elsif object.resource.present?
